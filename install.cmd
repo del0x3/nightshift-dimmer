53fa1f4f8039903c1af768aa2d5eef2507568815
Hlib Sorvenkov <hlibsorvenkov@outlook.com>
2026-09-16 00:03:17 +0300

    perf: eliminate Windows startup delay via registry in install.cmd

--- install.cmd
+++ install.cmd
@@ -10,6 +10,9 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "NightModeServic
 :: Create Startup shortcut via PowerShell
 powershell -NoProfile -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut([Environment]::GetFolderPath('Startup') + '\AutoNightMode.lnk'); $s.TargetPath = '%~dp0NightModeService.exe'; $s.WorkingDirectory = '%~dp0'; $s.Save()" >nul 2>&1
 
+:: Remove Windows autostart delay for instantaneous startup
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Serialize" /v "StartupDelayInMSec" /t REG_DWORD /d 0 /f >nul 2>&1
+
 echo [NightMode] Starting background service...
 start "" "%~dp0NightModeService.exe"
 
